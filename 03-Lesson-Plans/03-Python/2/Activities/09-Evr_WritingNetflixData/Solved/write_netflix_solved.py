19f2af35f5a68171687e174e9f0675a4c4aabef7
AMGonz96 <62735082+AMGonz96@users.noreply.github.com>
2021-12-22 21:12:59 -0500

    Add files via upload

--- 03-Lesson-Plans/03-Python/2/Activities/09-Evr_WritingNetflixData/Solved/write_netflix_solved.py
+++ 03-Lesson-Plans/03-Python/2/Activities/09-Evr_WritingNetflixData/Solved/write_netflix_solved.py
@@ -9,7 +9,7 @@ video = input("What show or movie are you looking for? ")
 csvpath = os.path.join("..", "Resources", "netflix_ratings.csv")
 
 # Specify the file to write to the movie data.
-
+txtpath = os.path.join("..", "output", "netflix_data.txt")
 
 # Set a variable to false to check if we found the video
 found = False
@@ -22,20 +22,24 @@ with open(csvpath, newline="") as csvfile:
     for row in csvreader:
         if row[0] == video:
 
-            # Set the variable created in Step 1 to confirm we have found the video
+            # Set the variable created to confirm we have found the video
             found = True
- 
+
             # Open the file using "write" mode. Specify the variable to hold the contents
-            
+            with open(txtpath, 'w') as textfile:
                 
                 # Create a variable to hold the title, rating level, and the movie rating inside parentheses.
-               
+                netflix_data = (
+                    f"Title: {row[0]}\n"
+                    f"Rating Level: {row[1]}\n"
+                    f"Rated: {row[5]}\n"
+                    )
                 
                 # Write the movie data to the text file.
-                
+                textfile.write(netflix_data)
 
                 # Print the movie data to the console.
-                
+                print(netflix_data)
 
             # Stop at first results to avoid duplicates
             break
